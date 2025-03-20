ee418a11adcff78aab86660331a0dbeba2ac22ec
Christophe Jauffret <reg-github@geo6.net>
2025-03-20 14:21:16 +0100

    update google uuid to 1.6.0

--- utils/utils.go
+++ utils/utils.go
@@ -46,7 +46,7 @@ func DebugResponse(res *http.Response) {
 func ConvertMapString(o map[string]interface{}) map[string]string {
 	converted := make(map[string]string)
 	for k, v := range o {
-		converted[k] = fmt.Sprintf(v.(string))
+		converted[k] = fmt.Sprint(v)
 	}
 	return converted
 }
